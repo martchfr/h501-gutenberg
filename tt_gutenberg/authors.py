46bd4ad203fa9e8fe551ecbb602cb36518257493
Christian Martinez <martchfr@iu.edu>
2025-10-13 23:48:30 -0400

    midnight updates, wayward, and vibes *suprised pichachu face*

--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -1,9 +1,21 @@
 import pandas as pd
 
-def read_dataframe(csv):
-    return pd.read_csv(csv)
+def list_authors():
 
-def list_authors(by_languages, alias):
+    # Setup and read-in all CSV files
+    gutenberg_authors = r"C:\Users\martchfr\OneDrive - Indiana University\Graduate School\MIS\INFO-H 501\Projects\h501-gutenberg\tt_gutenberg\gutenberg_authors.csv"
+    gutenberg_metadata = r"C:\Users\martchfr\OneDrive - Indiana University\Graduate School\MIS\INFO-H 501\Projects\h501-gutenberg\tt_gutenberg\gutenberg_metadata.csv"
+    authors_df = pd.read_csv(gutenberg_authors)
+    metadata_df = pd.read_csv(gutenberg_metadata)
 
+    # Merge Dataframes
+    metadata_w_authors_df = pd.merge(metadata_df, authors_df, on="gutenberg_author_id")
+    metadata_w_authors_df["translation_count"] = 1
 
-    return 
+    grouped_df = metadata_w_authors_df.groupby(
+        ["gutenberg_author_id", "alias"], as_index=False
+        ).agg(sum_translation_count=("translation_count", "sum")).sort_values(by="sum_translation_count", ascending=False)
+
+    author_list_descending = list(grouped_df["alias"])
+        
+    return author_list_descending
